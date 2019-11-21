6467c49269c837b84f9d293bfcb4c2649f5d30ee
Jacques Visser <jacquesvisserjnr@gmail.com>
2019-11-21 23:57:45 +0000

    Added parallax background

---
+++ Assets/parallaxing.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+public class parallaxing : MonoBehaviour
+{
+    public Transform[] backgrounds; 
+    private float[] parallaxScales;
+    public float smoothing = 1f; 
+    private Transform cam; 
+    private Vector3 previousCamPos; 
+                                   
+    void Awake()
+    {
+        // set up camera the reference
+        cam = Camera.main.transform;
+    }
+    // Use this for initialization
+    void Start()
+    {
+        // The previous frame had the current frame's camera position
+        previousCamPos = cam.position;
+        // asigning coresponding parallaxScales
+        parallaxScales = new float[backgrounds.Length];
+        for (int i = 0; i < backgrounds.Length; i++)
+        {
+            parallaxScales[i] = backgrounds[i].position.z;
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        // for each background
+        for (int i = 0; i < backgrounds.Length; i++)
+        {
+            // the parallax is the opposite of the camera movement because the previous frame multiplied by the scale
+            float parallax = (cam.position.x - previousCamPos.x) * parallaxScales[i];
+            // set a target x position which is the current position plus the parallax
+            float backgroundTargetPosX = backgrounds[i].position.x + parallax;
+            // create a target position which is the background's current position with it's target x position
+            Vector3 backgroundTargetPos = new Vector3(backgroundTargetPosX, backgrounds[i].position.y, backgrounds[i].position.z);
+            // fade between current position and the target position using lerp
+            backgrounds[i].position = Vector3.Lerp(backgrounds[i].position, backgroundTargetPos, smoothing * Time.deltaTime);
+        }
+        // set the previousCamPos to the camera's position at the end of the frame
+        previousCamPos = cam.position;
+    }
+}
