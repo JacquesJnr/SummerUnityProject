6ba2a83c9386be85d35a0a6587d45e37e4ddffd1
Jacques Visser <jacquesvisserjnr@gmail.com>
2019-12-09 15:23:14 +0000

    Added Parallax backgrounds and snow particles

---
+++ Assets/Scripts/Testing.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Testing : MonoBehaviour
+{
+    private FunctionTimer functionTimer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        FunctionTimer.Create(TestingAction, 3f);    
+    }
+
+    private void TestingAction()
+    {
+        Debug.Log("Testing");
+    }
+}
